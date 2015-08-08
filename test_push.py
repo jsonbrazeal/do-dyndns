2c6225e67ccff8c81f3c85aa725c95f7cf732af6
Jason Brazeal <jsonbrazeal@gmail.com>
2015-08-08 10:51:54 -0400

    added prints

--- test_push.py
+++ test_push.py
@@ -1,11 +1,16 @@
 from gcm import *
 
 with open('./api_key.txt', 'r') as f:
-    gcm = GCM(f.read())
+    api_key = f.read()
+    gcm = GCM(api_key)
+    print('got api_key: {}'.format(api_key))
 
 with open('./device_registration_id.txt', 'r') as f:
     reg_id = f.read()
+    print('got reg_id: {}'.format(reg_id))
 
 data = {'the_message': 'You have x new friends', 'param2': 'value2'}
 
+print('sending...')
 gcm.plaintext_request(registration_id=reg_id, data=data)
+print('sent...')
