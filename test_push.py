9d1ced4e299fb24b47916ad27e7cccd8106469e1
Jason Brazeal <jsonbrazeal@gmail.com>
2015-08-08 12:43:36 -0400

    removed python-gcm package; just using requests

--- test_push.py
+++ test_push.py
@@ -1,16 +1,30 @@
-from gcm import *
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import json
+import requests
 
 with open('./api_key.txt', 'r') as f:
     api_key = f.read()
-    gcm = GCM(api_key)
-    print('got api_key: {}'.format(api_key))
+    print('got api_key: {}'.format(api_key), end='\n\n')
 
 with open('./device_registration_id.txt', 'r') as f:
     reg_id = f.read()
-    print('got reg_id: {}'.format(reg_id))
+    print('got reg_id: {}'.format(reg_id), end='\n\n')
 
-data = {'the_message': 'You have x new friends', 'param2': 'value2'}
+data = {'message': 'hi'}
 
 print('sending...')
-gcm.plaintext_request(registration_id=reg_id, data=data)
-print('sent...')
+
+headers = {'Authorization': 'key={}'.format(api_key),
+           'Content-Type': 'application/json'}
+data = {'registration_ids': [reg_id],
+        'data': {'message': 'test!' }}
+print('headers={}'.format(headers), end='\n')
+print('data={}'.format(data), end='\n\n')
+response = requests.post('https://android.googleapis.com/gcm/send', headers=headers, data=json.dumps(data))
+print('sent...', end='\n\n')
+print('response=')
+print(response.text, end='\n\n')
